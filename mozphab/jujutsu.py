1d164577f0dfa2069fc25048cce0372106b4f8d1
Erich Gubler <erichdongubler@gmail.com>
2025-04-09 14:47:57 -0400

    fix(jujutsu): use `jj git root` for Git repo detection and `moz-phab` ANYWHERE :tada:

--- mozphab/jujutsu.py
+++ mozphab/jujutsu.py
@@ -40,40 +40,9 @@ class Jujutsu(Repository):
     # ----
 
     def __init__(self, path: str):
-        # NOTE: We expect a co-located Git repository alongside the Jujutsu repository, so let's
-        # start up a Git backend to handle the pieces we need.
-
-        # Since there could be multiple Jujutsu workspaces, and the underlying
-        # Git repository could be in either one of them, or in another folder
-        # entirely, we will need to do a small search first.
-
-        self.git_path = Path(path)
-        repo = self.git_path / ".jj" / "repo"
-        if repo.is_file():
-            # NOTE: This should point to the root of the repository, with the `.jj` directory right
-            # inside.
-            self.git_path = Path(repo.read_text()).parent.parent
-        git_target = self.git_path / ".jj" / "repo" / "store" / "git_target"
-        if git_target.is_file():
-            store_dir_path = git_target.parent
-            git_repo_dir_path = Path(git_target.read_text())
-            ugly_git_dir_path = store_dir_path.joinpath(git_repo_dir_path)
-            ugly_git_repo_path = (
-                ugly_git_dir_path.parent
-            )  # slice off the `.git` segment
-            pretty_git_repo_path = ugly_git_repo_path.resolve()
-            self.git_path = pretty_git_repo_path
+        self.git_path = Path(check_output(["jj", "git", "root"], split=False))
         logger.debug(f"git_path: {self.git_path}")
-
-        if (self.git_path / ".git").is_dir():
-            self.__git_repo = Git(self.git_path)
-        else:
-            # TODO: Do we actually only support co-located Git repos now? 🤔
-            raise Error(
-                "Underlying Git repository for Jujutsu could not be found. "
-                "Make sure you are using a colocated Git repository and not "
-                "the pure Jujutsu backend."
-            )
+        self.__git_repo = Git(self.git_path.parent)
 
         # Populate common fields expected from a `Repository`
 
@@ -203,12 +172,7 @@ class Jujutsu(Repository):
             ) = log_line.split("\n", 7)
             desc = desc.splitlines()
 
-            tree_hash = check_output(
-                ["git", "rev-parse", commit_id + ":./"],
-                split=False,
-                strip=False,
-                cwd=self.git_path,
-            ).rstrip()
+            tree_hash = self.__git_repo._revparse(commit_id + ":./")
 
             parents = parents.split(" ")
             if first_commit_id != commit_id and len(parents) > 1:
